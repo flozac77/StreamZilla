58fd714cfabd992b0d194d8437fa66c3cb5cd7de
flozac77 <zacchero@hotmail.fr>
2025-05-10 11:28:28 +0200

    fix: mise à jour de la configuration Vercel pour le backend et ajout d'une page d'accueil HTML

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,25 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+html = f"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FastAPI on Vercel</title>
+        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+    </head>
+    <body>
+        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
+            <h1>Hello from FastAPI@{__version__}</h1>
+            <ul>
+                <li><a href="/docs">/docs</a></li>
+                <li><a href="/redoc">/redoc</a></li>
+            </ul>
+            <p>Powered by <a href="https://vercel.com" target="_blank">Vercel</a></p>
+        </div>
+    </body>
+</html>
+"""
 # Include routers
 app.include_router(router, prefix=settings.API_V1_STR)
 
