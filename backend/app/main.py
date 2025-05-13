1f24c22f7047856dd2270d5d3fd551a6c8e8b926
flozac77 <zacchero@hotmail.fr>
2025-05-13 19:48:01 +0200

    fix: mise à jour de la configuration CORS pour autoriser toutes les origines - Changement de la configuration CORS pour permettre toutes les origines en remplaçant `settings.CORS_ORIGINS` par `["*"]` dans `main.py`.

--- backend/app/main.py
+++ backend/app/main.py
@@ -64,7 +64,7 @@ app = FastAPI(
 # Configuration CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
