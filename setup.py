fbee981d9c3fdb9299fb5fb638dde388acf75c8d
flozac77 <zacchero@hotmail.fr>
2025-05-21 14:32:08 +0200

    fix: mise à jour des fichiers de configuration et des dépendances - Ajout de .vercel et node_modules dans .gitignore. - Mise à jour des versions des dépendances dans requirements.txt pour assurer la compatibilité. - Suppression de backend/requirements.txt, les dépendances sont maintenant centralisées. - Nettoyage de setup.py en retirant les dépendances obsolètes.

--- setup.py
+++ setup.py
@@ -4,13 +4,5 @@ setup(
     name="visibrain",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        "fastapi",
-        "uvicorn",
-        "pymongo",
-        "python-dotenv",
-        "httpx",
-        "fastapi-cache2",
-    ],
     python_requires=">=3.8,<3.13",
 ) 
